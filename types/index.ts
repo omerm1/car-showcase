c9c0a1e5bbe09f41ccdaaa7ceab779f83a61e8cd
Omer M <omerminhas1@gmail.com>
2023-06-12 16:16:07 +0100

    added searchbar and autocomplete

--- types/index.ts
+++ types/index.ts
@@ -6,4 +6,19 @@ export interface CustomButtonProps {
   handleClick?:
   MouseEventHandler<HTMLButtonElement>;
   btnType: "button" | "submit";
-}
+};
+
+export interface SearchManufacturerProps {
+  manufacturer: string;
+  setManufacturer: (manufacturer: string) => void;
+};
+
+export interface OptionProps {
+  title: string;
+  value: string;
+};
+
+export interface CustomFilterProps {
+  title: string;
+  options: OptionProps[];
+};
