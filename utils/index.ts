520e48c3d50c5873187495faafc0098090eedaa5
Omer M <omerminhas1@gmail.com>
2023-06-13 09:01:57 +0100

    added card details and imagin api

--- utils/index.ts
+++ utils/index.ts
@@ -1,6 +1,8 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars(){
   const headers = {
-    'X-RapidAPI-Key': '629ed3b216msh385ee84dea839d2p183de2jsnbe7776b79308',
+    'X-RapidAPI-Key': process.env.NEXT_PUBLIC_RAPID_API_KEY || "",
     'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
   }
 
@@ -25,3 +27,18 @@ export const calculateCarRent = (city_mpg: number, year: number) => {
 
   return rentalRatePerDay.toFixed(0);
 };
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, model, year } = car;
+
+  url.searchParams.append('customer', process.env.NEXT_PUBLIC_IMAGIN_API_KEY || '');
+  url.searchParams.append('make', make);
+  url.searchParams.append('modelFamily', model.split(" ")[0]);
+  url.searchParams.append('zoomType', 'fullscreen');
+  url.searchParams.append('modelYear', `${year}`);
+  // url.searchParams.append('zoomLevel', zoomLevel);
+  url.searchParams.append('angle', `${angle}`);
+
+  return `${url}`;
+} 
